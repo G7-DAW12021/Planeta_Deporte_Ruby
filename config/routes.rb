350d8a096450ae770ca11e19fe689201eafaea1f
Luzma <lzmps106@outlook.es>
2021-12-06 13:22:50 +0000

    Instalacion ruby y CRUD de usuarios

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'users/create'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/delete'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
