e417873435fb8a139393fde0d2ce7fe8a1534ad6
Javier <jasua2000@gmail.com>
2021-12-08 11:08:19 +0000

    v1.0.2

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   get 'home/index'
 
   #----------------------------------Article Routes -----------------------------------------#
+  resources :articles
   get 'articles', to: 'articles#index'
   get 'articles/new', to: 'articles#new'
   get 'articles/:id', to: 'articles#show'
   get 'articles/:id/edit', to: 'articles#edit'
-  patch 'articles/:id', to: 'articles#update', as: :article
+  #patch 'articles/:id', to: 'articles#update', as: :article
   post 'articles', to: 'articles#create'
   delete 'articles/:id', to: 'articles#destroy'
 
