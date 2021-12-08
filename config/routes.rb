3c982b49e4aa174ba3aa5377ee9ff68ab0c01380
Javier <jasua2000@gmail.com>
2021-12-08 10:37:03 +0000

    v1.0.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  #----------------------------------Home Routes --------------------------------------------#
   root to: 'home#index'
   get 'home/index'
+
+  #----------------------------------Article Routes -----------------------------------------#
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id/edit', to: 'articles#edit'
+  patch 'articles/:id', to: 'articles#update', as: :article
+  post 'articles', to: 'articles#create'
+  delete 'articles/:id', to: 'articles#destroy'
+
+  #----------------------------------Comment Routes -----------------------------------------#
+
+
+  #----------------------------------User Routes -----------------------------------------#
 
 
 
