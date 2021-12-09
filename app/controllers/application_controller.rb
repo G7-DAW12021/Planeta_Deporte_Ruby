6c31f812714934297912b4e56faa877909ede3d3
Luzma <lzmps106@outlook.es>
2021-12-09 00:12:56 +0000

    Instalación de device y sus vistas. La migración de la tabla de usuarios con todos los campos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :login
+  helper_method :current_user
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def login
+    !current_user.nil?
+  end
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,67 @@
 class UsersController < ApplicationController
-  def create
+  before_action :is_user, only: [:show, :edit, :update, :destroy] #Acciones que manda a llamar is_user
+  def new
+    @user= User.new
+  end
 
+  def create
   end
 
   def show
-
   end
 
   def edit
+  end
 
+  def update
   end
 
   def delete
+    @user.destroy
+    redirect_to root_path
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def is_user
+    @user = User.find_by_id(params[:id])
   end
 end
+=begin
+def create
+  @user = User.new(user_params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  else
+    render :new
+  end
+end
+
+def edit
+  if !login || !@user
+    redirect_to root_path
+  end
+end
+
+def update
+  if @user
+    @user.update(user_params)
+
+    if @user.errors.any?
+      render "edit"
+    else
+      redirect_to user_path(@user)
+    end
+  else
+    render "edit"
+  end
+end
+=end
+
+
+
